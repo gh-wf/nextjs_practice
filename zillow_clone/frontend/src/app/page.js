6041d77171514c1bc0dce0367e49cfa276afc462
Wayne <wayneferguson@gmail.com>
2024-11-06 12:40:11 -0500

    added query (without .env)

--- zillow_clone/frontend/src/app/page.js
+++ zillow_clone/frontend/src/app/page.js
@@ -3,7 +3,46 @@ import SearchBar from "./components/SearchBar"
 import Map from "./components/Map"
 import Card from "./components/Card"
 
-const Home = () => {
+const getProperties = async() => {
+  const HYGRAPH_ENDPOINT = process.env.HYGRAPH_ENDPOINT
+  if (!HYGRAPH_ENDPOINT) {
+    throw new Error("HYGRAPH_ENDPOINT is not set")
+  }
+  const response = await fetch(HYGRAPH_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query: `
+        query Properties {
+          properties {
+            beds
+            description
+            images {
+              fileName
+              url
+            }
+            location {
+              latitude
+              longitude
+            }
+            name
+            rentalPrice
+            slug
+            id
+          }
+        }
+        `
+    })
+  })
+  const json = await response.json()
+  return json.data.properties
+}
+
+const Home = async () => {
+    const properties = await getProperties()
+    console.log(properties)
   return (
    <> 
       <NavBar/>
